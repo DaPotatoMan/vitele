598b070d4d5fc115b3d03c2405c2f46ca5717de8
DaPotatoMan <iam.fahadul@gmail.com>
2021-07-23 15:11:47 +0600

    feat: Add windicss theme config

--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,26 @@
 import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
-   darkMode: false
+   darkMode: false,
+
+   theme: {
+      extend: {
+         colors: {
+            accent: '#536aff'
+         },
+
+         textColor: {
+            default: '#202020',
+            'default-light': '#454545',
+
+            inverse: '#fff'
+         },
+
+         backgroundColor: {
+            default: '#f8f6f5',
+            'default-elevated': '#fff',
+            inverse: '#000'
+         }
+      }
+   }
 });
